a2ab4f253f3cee161a1b80270bf7ae3361dc8810
Marc <marcrlai@gmail.com>
2026-04-11 22:44:42 -0400

    Initial Commit

---
+++ PoolLab2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PoolLab2
+//
+//  Created by Marc Lai on 4/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PoolLab2/PoolLab2App.swift
@@ -0,0 +1,17 @@
+//
+//  PoolLab2App.swift
+//  PoolLab2
+//
+//  Created by Marc Lai on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct PoolLab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
